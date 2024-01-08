80c01957c983d7f0c8c1d00caf92f2128ec31af7
Jandre Cronje <jandrecronje@outlook.com>
2024-01-08 20:45:01 +1300

    Commit Initilize
    start project setup

---
+++ BatmanRemake/Source/BatmanRemake/BatmanRemake.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BatmanRemake.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BatmanRemake, "BatmanRemake" );
---
+++ BatmanRemake/Source/BatmanRemake/BatmanRemakeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BatmanRemakeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BATMANREMAKE_API ABatmanRemakeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
